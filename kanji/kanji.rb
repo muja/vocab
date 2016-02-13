2bbc55c64466cfca6484f459e255d2344ae93468
Danyel Bayraktar <cydrop@gmail.com>
2016-02-13 23:30:54 +0100

    Initial commit. Add kanji from lectures 1 through 8 Add compounds from lectures 1 through 8 Make scripts that process these files for Anki import. Use kanji vector graphics when adding them to Anki.

---
+++ kanji/kanji.rb
@@ -0,0 +1,81 @@
+#!/usr/bin/env ruby
+
+require 'pathname'
+require 'csv'
+require 'json'
+require 'hashie'
+
+Dir.chdir Pathname.new(__FILE__).parent
+Dir.mkdir "out" unless Dir.exist? "out"
+
+FILE_TEMPLATE = Pathname.new(Dir.pwd).parent.join("kanjivg", "kanji", "0%s.svg").to_s
+
+characters = if File.exists? "out/kanjidata.json"
+  Hashie::Mash.new(JSON.parse(File.read("out/kanjidata.json")))
+else
+  puts "Fetching kanji data..."
+  require 'open-uri'
+  require 'nokogiri'
+
+  # FOR LICENSE see http://www.csse.monash.edu.au/~jwb/kanjidic2/
+  xml = open("http://www.csse.monash.edu.au/~jwb/kanjidic2/kanjidic2.xml.gz")
+  i = 0
+  Nokogiri::XML(xml).css('character').inject({}) do |h, char|
+    print "\r"
+    print i += 1
+    kanji = char.css("literal").text
+    on = char.css("reading[r_type=ja_on]").map(&:text).join(" ")
+    kun = char.css("reading[r_type=ja_kun]").map(&:text).join(" ")
+    meaning = char.css("meaning:not([m_lang])").map(&:text).join(", ")
+    h.merge kanji => Hashie::Mash.new(
+      kanji: kanji,
+      on: on,
+      kun: kun,
+      meaning: meaning
+    )
+  end.tap do |characters|
+    puts
+    license = open("http://www.edrdg.org/edrdg/licence.html").read
+    File.write("out/kanjidata.license.html", license)
+
+    File.write("out/kanjidata.json", JSON.pretty_generate(characters))
+  end
+end
+
+by_file = {}
+all = Dir["in/**/*"].map do |file|
+  next [] unless File.exists?(file)
+  lecture = File.read(file).strip
+  by_file[file] = CSV.parse(lecture, col_sep: ";").map do |kanji, meaning, on, kun|
+    if char = characters[kanji]
+      on = char.on
+      kun = char.kun
+      meaning_en = char.meaning
+    else
+      puts "#{kanji}: Not found! (in #{file}), on: #{on}, kun: #{kun}"
+      on ||= ""
+      kun ||= ""
+    end
+    Hashie::Mash.new(
+      kanji: kanji,
+      on: on.split.join("; "),
+      kun: kun.split.map do |kun|
+        real, additional = kun.split(".")
+        additional ? "<u>#{real}</u>#{additional}" : kun
+      end.join("; "),
+      meaning_de: meaning,
+      meaning_en: meaning_en,
+      svg: FILE_TEMPLATE % kanji.ord.to_s(16),
+    )
+  end
+end.flatten
+
+by_file.merge("all" => all).each do |infile, vocabs|
+  csv = CSV.generate(col_sep: ";") do |csv|
+    vocabs.each do |vocab|
+      csv << vocab.values
+    end
+  end
+  out = File.basename(infile, File.extname(infile))
+  File.write "out/#{out}.csv", csv
+end
